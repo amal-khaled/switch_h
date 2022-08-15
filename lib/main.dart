6024ce40b93df114d3dc4c0fa4db5a8ad06612d8
nermeen841 <63563785+nermeen841@users.noreply.github.com>
2022-08-15 14:52:39 +0200

    drawer

--- lib/main.dart
+++ lib/main.dart
@@ -237,7 +237,8 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Scaffold(
           key: homeScaffoldKey,
           backgroundColor: Colors.white,
-          drawer: SafeArea(
+          drawer: SizedBox(
+           height: MediaQuery.of(context).size.height,
             child: Drawer(
               // Add a ListView to the drawer. This ensures the user can scroll
               // through the options in the drawer if there isn't enough vertical
